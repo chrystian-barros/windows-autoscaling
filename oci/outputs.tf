e549db3821a3a6b1cee1e88d7d5d5c49b6eeeee3
chrystian.barros@terceiro-sky.com.br <chrystian.barros@terceiro.sky.com.br>
2026-06-27 15:10:44 -0300

    Added more outputs for shared file system

--- oci/outputs.tf
+++ oci/outputs.tf
@@ -1,3 +1,14 @@
-output "mount_target_ip" {
+output "mount_target_ip_address" {
   value = oci_file_storage_mount_target.mount_target.ip_address
+  description = "IPv4 addres of the mount target. Used to mount the file system."
+}
+
+output "file_system_path" {
+  value = oci_file_storage_export.export.path
+  description = "File system path to be mounted in external systems."
+}
+
+output "powershell_file_system_path" {
+  value = oci_file_storage_export.powershell_modules_export.path
+  description = "File system path for PowerShell modules and scripts."
 }
